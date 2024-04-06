6814d1bf21e150360bf047027c2effbecc777073
Sajhith Begambre <s.begambrepoveda@dxc.com>
2024-04-06 14:14:43 -0500

    Cambios de sajhith

--- src/test/java/starter/CatalogTestSuite.java
+++ src/test/java/starter/CatalogTestSuite.java
@@ -7,9 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberSerenityRunner.class)
 @CucumberOptions(
         plugin = {"pretty"},
-        //features = "src/test/resources/features/card",
-        //features = "src/test/resources/features/Shopping/Write&ReadExcel.feature",
-        features = "src/test/resources/features/Documents/ReadDocuments.feature",
+        features = "src/test/resources/features",
         tags = "@Test"
 )
 public class CatalogTestSuite {
--- src/test/resources/features/SauceLabs/Login.feature
+++ src/test/resources/features/SauceLabs/Login.feature
@@ -1,8 +1,8 @@
 Feature: Login
 
   Rule: Customer need to provide valid credentials to access the product catalog
-
-      Example: Colin log in with valid credentials
+  @Test
+      Scenario: : Colin log in with valid credentials
         Given Colin is on the login page
         When Colin logs in with valid credentials
         Then she should be presented with the product catalog
